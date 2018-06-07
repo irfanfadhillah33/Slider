8a3d73cb63a1ec5a21ff8e0a3c1c41a8f90d7871
irfanfadhillah33 <39149055+irfanfadhillah33@users.noreply.github.com>
2018-06-08 06:15:44 +0700

    update jumat 2

--- file/footer.php
+++ file/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
     <footer class="py-0 footer_bg">
       <div class="container">
-        <p class="m-0 text-center text-black" align="center">Copyright &copy; Teitra Mega 2018</p>
+        <p class="m-0 text-center text-white" align="center">Copyright &copy; Teitra Mega 2018</p>
       </div>
       </footer>
       <!-- /.container -->
